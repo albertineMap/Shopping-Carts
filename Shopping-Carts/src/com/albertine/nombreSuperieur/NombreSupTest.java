79564194b74a80e4d1ab76d31580d2e14f745f92
albertine.teugueu <ateugueu@dept-info.crosemont.quebec>
2017-12-04 21:22:04 -0500

    Ajout d'un test(utilisateur entre un nombre et obtient le nombre superieur suivant) dans la classse NombreSupTest

--- Shopping-Carts/src/com/albertine/nombreSuperieur/NombreSupTest.java
+++ Shopping-Carts/src/com/albertine/nombreSuperieur/NombreSupTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package nombresuperieur;
+package com.albertine.nombreSuperieur;
 
 import java.util.Scanner;
 
